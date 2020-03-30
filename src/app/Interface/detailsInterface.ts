71623896676c6cdefb7b0f3d1d564614aebe261d
Naveen132895 <veeranaveen.sethuraman@faceacademy.in>
2020-03-30 11:46:59 +0530

    first commit

---
+++ src/app/Interface/detailsInterface.ts
@@ -0,0 +1,4 @@
+export interface listDetails{
+    name : string;
+    comments : string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { listDetails} from './Interface/detailsInterface';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Article-with-comments';
+  
+  details : listDetails[] =[
+    { name : 'Pravesh', comments : 'In my academics, I learnt only theoretical concepts of programming but ProGrad emphasized my coding knowledge by teaching me a different approach in solving all the coding problems. My mentor offered me excellent support by guiding us on each step to improve my progress.' },
+    { name : 'Subham', comments : 'I am from Electronics and Communication Engineering background. Before starting this course, I couldn’t compile a code properly. Now I can solve complex problems. I have good knowledge of all the programming languages I have learnt.'}
+  ];
+  name : string ='';
+  comment : string = '';
+
+
+  getValues(name:string, comment:string) {
+
+    this.name = name;
+    this.comment = comment;
+
+    this.details.push({"name" : name, "comments" : comment});
+    console.log("Success");
+
+  }
+  
 }
+
+
